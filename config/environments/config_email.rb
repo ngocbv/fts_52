b0a80196f53ff2607b573260a675bf3762ffd664
NeverSmilek57CLC <neversmile12b1@gmail.com>
2016-02-24 09:00:23 +0700

    refs #54610 reset password using devise

---
+++ config/environments/config_email.rb
@@ -0,0 +1,15 @@
+Rails.application.configure do
+  # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+
+  # SMTP settings for gmail
+  ActionMailer::Base.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: "587",
+    authentication: :plain,
+    user_name: ENV["gmail_username"],
+    password: ENV["gmail_password"],
+    enable_starttls_auto: true
+  }
+end
